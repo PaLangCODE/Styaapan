5569ed0b7fe4218e1b87eb9e264442de6cc2bed6
Drexter-07 <59890906+Drexter-07@users.noreply.github.com>
2022-04-10 17:10:49 +0530

    Minor Changes

--- CONTRACT/config.js
+++ CONTRACT/config.js
@@ -1,3 +1,3 @@
 export const nftAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 export const nftTransferAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-export const rpc_url = `https://rpc-mainnet.maticvigil.com/v1/0a90c8fad2e706c9b1813e2b5f3bd0c209fbb3f1`;
+export const rpc_url = `https://rpc-mumbai.maticvigil.com/v1/3563bdf92c4890518396bd186677a6bc37e727f3`;
